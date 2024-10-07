da7f953d188ed1ec0d20966d3041b10f03f40cef
Letícia Fritz <161434060+letfritz@users.noreply.github.com>
2024-10-06 21:47:58 -0300

    upload/app-atestimonials

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Experience from "./components/Experience"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
-import Testimonial from "./components/Testimonial"
 
 const App = () => {
   return(
